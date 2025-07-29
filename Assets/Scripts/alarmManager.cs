ee36f67c4e32e2008d07d2eb018147d8eadba214
SAE1\1034241 <1034241@student.sae.edu>
2025-07-29 16:16:32 +1000

    renamed gameManager to alarmManager added monobehaviour to alarm manager

--- Assets/Scripts/alarmManager.cs
+++ Assets/Scripts/alarmManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class gameManager 
+public class alarmManager : MonoBehaviour 
 {
 
     public  UnityEvent alarmActive;
