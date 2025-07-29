d6be6e571d7999b0c870c944d4c69fa2a0f310f3
SAE1\1034241 <1034241@student.sae.edu>
2025-07-29 15:48:09 +1000

    Adding basic Game Manager script

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class gameManager 
+{
+
+    public static UnityEvent alarmActive;
+    public static UnityEvent posLastSeen;
+}
