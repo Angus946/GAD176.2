daf2bfb41090453367d6552994699f0857e2b334
Hugh <1035084@student.sae.edu.au>
2025-08-17 14:41:34 +1000

    Refined Alarm Manager
    We cut the event system because it was annoying to work with.
    Drag this into an empty in the scene for the code to function.

---
+++ alarmManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alarmManager : MonoBehaviour 
+{
+    //HHB Wrote This Script
+
+    public bool alarmActive = false;
+    public Transform lastPositionSeen;
+    public AudioSource alarmSound;
+    public bool alarmSoundActive;
+    public float alarmTimer = 0f;
+    public float alarmTimerMax = 0f;
+
+    void Start()
+    {
+    alarmSound = GetComponent<AudioSource>();
+
+        if (alarmSound != null)
+        {
+            alarmSound.loop = true;
+        }
+        else
+        {
+        Debug.LogWarning("No AudioSource found! Go fix it! Null Error!");
+        }
+    }
+
+    void Update()
+    {
+        //Alarm is Active
+        if (alarmActive == true)
+        {
+            alarmTimer += Time.deltaTime;
+            if (alarmSoundActive == false)
+            {
+                alarmSound.Play();
+                alarmSoundActive = true;
+            }
+        }
+
+        //Alarm is Inactive
+        else //sads
+        {
+            alarmSound.Stop();
+            alarmSoundActive = false;
+        }
+
+        //Reset the Alarm after Max Exceeded and Disable Alarm
+        if (alarmTimer >= alarmTimerMax && alarmActive == true)
+        {
+            alarmActive = false;
+            alarmTimer = 0f;
+        }   
+    }
+}
